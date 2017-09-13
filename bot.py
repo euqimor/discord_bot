5c0ab40871615a705b036bf8bc79173773788d0b
euqimor <romique@gmail.com>
2017-09-14 00:25:52 +0300

    work-in-progress

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ bot = commands.Bot(command_prefix='?', description=description)
 
 
 def load_game_suggestions():
+    '''
+    :return: tries to load a dict of suggestions from a file,
+    otherwise returns empty dict
+    '''
     suggestions = {}
     os.chdir(os.path.expanduser('~/bothelper/'))
     try:
@@ -46,8 +50,6 @@ def load_data(filename):
     except:
         print('Something went wrong during data evaluation')
 
-
-
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -82,46 +84,6 @@ async def suggest(data):
     await bot.say(str(suggestions))
     save_data(suggestions,'suggestions')
 
-
-@bot.command()
-async def roll(dice : str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await bot.say('Format has to be in NdN!')
-        return
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await bot.say(result)
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(*choices : str):
-    """Chooses between multiple choices."""
-    await bot.say(random.choice(choices))
-
-@bot.command()
-async def repeat(times : int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await bot.say(content)
-
-@bot.command()
-async def joined(member : discord.Member):
-    """Says when a member joined."""
-    await bot.say('{0.name} joined in {0.joined_at}'.format(member))
-
-@bot.group(pass_context=True)
-async def cool(ctx):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
-
-@cool.command(name='bot')
-async def _bot():
-    """Is the bot cool?"""
-    await bot.say('Yes, the bot is cool.')
 
 if __name__ == '__main__':
     suggestions = load_game_suggestions()
