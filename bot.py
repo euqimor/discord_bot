f14f5b0d2c7c3592396c8b00df2c1f74095faafd
euqimor <romique@gmail.com>
2017-10-08 19:35:08 +0300

    check_admin_rights fix

--- bot.py
+++ bot.py
@@ -13,14 +13,11 @@ bot = commands.Bot(command_prefix='$', description=description)
 
 async def check_admin_rights(ctx):
     try:
-        roles_list = ctx.author.roles
+        roles = ctx.author.roles
     except AttributeError:
         await ctx.send('Something went wrong. If you tried this command in a DM, the bot '
                        'doesn\'t know how to check if you have admin rights.')
         return None
-    roles = []
-    for role in roles_list:
-        roles.append(role.name)
     success_flag = 0
     for role in roles:
         if role.permissions.administrator:
