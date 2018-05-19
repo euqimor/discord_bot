1fc23f8bab11e7efc3f0e087174e2b334a427841
euqimor <romique@gmail.com>
2018-05-19 22:17:31 +0300

    token test -> prod

--- bot.py
+++ bot.py
@@ -75,6 +75,6 @@ if __name__ == '__main__':
         except Exception as e:
             print(f'Failed to load extension {extension}.')
     if check_database(bot.db_name):
-        bot.run(os.environ['BOT_TEST'])
+        bot.run(os.environ['BOT_PROD'])
     else:
         sys.exit(1)
