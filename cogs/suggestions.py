e08c0a8df04416acfd4dbac7656f4486a68121fa
euqimor <romique@gmail.com>
2018-05-23 04:57:43 +0300

    emoji test

--- cogs/suggestions.py
+++ cogs/suggestions.py
@@ -153,7 +153,7 @@ class SuggestionsCog:
     @commands.command()
     async def test(self, ctx):
         message = await ctx.send(':one: afdasf\n\n:two: aggag\n\n:three: asfasfa\n\n:four: afasfa')
-        await message.add_reaction('\u0031')
+        await message.add_reaction(u'1')
 
 
 def setup(bot):
