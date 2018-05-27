86efed75ac46406a932c6742be8e3a9f63924898
euqimor <romique@gmail.com>
2018-05-27 14:32:06 +0300

    $exec emoji

--- cogs/owner.py
+++ cogs/owner.py
@@ -92,7 +92,7 @@ class OwnerCog:
             temp_out = StringIO()
             try:
                 sys.stdout = temp_out
-                exec(code, globals(), locals())
+                await exec(code, globals(), locals())
                 await ctx.message.add_reaction(success_flag)
                 await ctx.send(f"```py\n{temp_out.getvalue()}\n```")
             except:
