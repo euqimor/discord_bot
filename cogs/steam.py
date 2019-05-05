b96b3c6164cc82e6eced743d039b533307b83f08
Romique <romique@gmail.com>
2019-05-06 00:18:07 +0300

    steam lower

--- cogs/steam.py
+++ cogs/steam.py
@@ -20,7 +20,7 @@ class SteamCog:
                 con.execute(f'CREATE TABLE IF NOT EXISTS Steam (app_id INT PRIMARY KEY , app_name TEXT)')
                 for app in r.json()['applist']['apps']:
                     app_id = app["appid"]
-                    app_name = app["name"]
+                    app_name = app["name"].lower()
                     con.execute(f'INSERT OR IGNORE INTO Steam (app_id, app_name) VALUES (?, ?);', (app_id, app_name))
 
     async def get_steam_apps_list_async(self):
@@ -34,14 +34,14 @@ class SteamCog:
                 con.execute(f'CREATE TABLE IF NOT EXISTS Steam (app_id INT PRIMARY KEY , app_name TEXT)')
                 for app in r_json['applist']['apps']:
                     app_id = app["appid"]
-                    app_name = app["name"]
+                    app_name = app["name"].lower()
                     con.execute(f'INSERT OR IGNORE INTO Steam (app_id, app_name) VALUES (?, ?);', (app_id, app_name))
 
     @commands.group(invoke_without_command=True)
     async def steam(self, ctx, *, app_name=''):
         if app_name == '':
             await ctx.send('Please provide name to search for')
-        app_name = app_name.strip()
+        app_name = app_name.strip().lower()
         if app_name[0] == '\"':
             app_name = app_name.strip('"')
         with closing(sqlite3.connect(self.bot.db_name)) as con:
@@ -56,20 +56,14 @@ class SteamCog:
                     if result:
                         app_id = result[0]
                         await ctx.send(f'https://store.steampowered.com/app/{app_id}/')
-                    else:  # try to capitalize the name sting
-                        app_name_capitalized = app_name.title()
-                        result = con.execute('SELECT app_id FROM Steam WHERE app_name=?', (app_name_capitalized,)).fetchone()
-                        if result:
-                            app_id = result[0]
-                            await ctx.send(f'https://store.steampowered.com/app/{app_id}/')
-                        else:  # if still not found, search for relevant names
-                            app_names_list = [x[0] for x in con.execute('SELECT app_name FROM Steam').fetchall()]
-                            matches = difflib.get_close_matches(app_name, app_names_list, cutoff=0.4, )
-                            if matches:
-                                matches = '\n'.join(matches)
-                                await ctx.send(f'Game `{app_name}` not found, did you mean:\n```\n{matches}\n```')
-                            else:
-                                await ctx.send(f'Game "{app_name}" not found')
+                    else:  # if still not found, search for relevant names
+                        app_names_list = [x[0] for x in con.execute('SELECT app_name FROM Steam').fetchall()]
+                        matches = difflib.get_close_matches(app_name, app_names_list, cutoff=0.4, )
+                        if matches:
+                            matches = '\n'.join(matches)
+                            await ctx.send(f'Game `{app_name}` not found, did you mean:\n```\n{matches}\n```')
+                        else:
+                            await ctx.send(f'Game "{app_name}" not found')
 
 
 def setup(bot):
